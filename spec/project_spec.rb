6d178de935b3674e04f9971aca8afa2f73f34658
gangelo <web.gma@gmail.com>
2018-08-06 07:24:14 -0400

    Prepare for aruba use

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -1,7 +1,13 @@
 RSpec.describe YardiConvertTools::Project do
+  let(:downloads_dir) { '~/Downloads' }
+  let(:jira_ticket) { 'XX-1234' }
+  let(:project_dir) { File.join(downloads_dir, jira_ticket) }
 
   context '.create_project' do
-    it 'should create the project'
+    it 'should create the project' do
+      YardiConvertTools::Project.create_project(jira_ticket)
+      expect(downloads_dir).to be_an_existing_path
+    end
   end
 
   context '.create_property' do
