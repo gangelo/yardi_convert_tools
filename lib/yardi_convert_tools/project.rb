1f54632cd7e19033c0ad08a4025f876537d551ae
gangelo <web.gma@gmail.com>
2018-08-10 10:14:22 -0400

    Refactor Project module to be a class

--- lib/yardi_convert_tools/project.rb
+++ lib/yardi_convert_tools/project.rb
@@ -5,87 +5,174 @@ require 'ostruct'
 require 'pry'
 
 module YardiConvertTools
-  # Helpers to handle the project.
-  module Project
+  # Class for projects.
+  class Project
     DEFAULT_ROOT_DIR = '/tmp'
-    DEFAULT_IN_WORK_DIR = '/in-work'
-    DEFAULT_COMPLETED_DIR = '/completed'
+    DEFAULT_DRY_DIR = '/dry'
+    DEFAULT_LIVE_DIR = '/live'
 
-    def self.create_project(jira_ticket)
-      project_root_dir = create_project_root_dir(jira_ticket)
-      raise StandardError, 'Unable to create project root directory'\
-        unless project_root_dir
-      project_subdirs = create_project_sub_dirs(project_root_dir)
-      create_project_struct(jira_ticket, project_root_dir, project_subdirs)
+    attr_reader :jira_ticket
+    attr_reader :property_ids
+    attr_reader :root_dir
+    attr_reader :dry_dir
+    attr_reader :live_dir
+    attr_reader :property_files
+
+    def initialize(jira_ticket, property_ids = [])
+      project = Project.create(jira_ticket, property_ids)
+      self.jira_ticket = project[:jira_ticket]
+      self.property_ids = project[:property_ids]
+      self.root_dir = project[:root_dir]
+      self.dry_dir = project[:dry_dir]
+      self.live_dir = project[:live_dir]
+      self.property_files = project[:property_files]
     end
 
-    # TODO: Get this from config file
-    def self.root_dir
+    # TODO: Load an existing project
+    def self.load(jira_ticket); end
+
+    def self.create(jira_ticket, property_ids = [])
+      project_root_dir = create_root_dir(jira_ticket)
+      project_subdirs = create_sub_dirs(project_root_dir)
+      property_files = create_property_files(jira_ticket, property_ids)
+      {
+        jira_ticket: normalize_jira_ticket(jira_ticket),
+        property_ids: property_ids,
+        root_dir: project_root_dir,
+        dry_dir: project_subdirs[:dry_dir],
+        live_dir: project_subdirs[:live_dir],
+        property_files: property_files
+      }
+    end
+
+    # The default root directory that all projects will be created under.
+    def self.default_root_dir
+      # TODO: Get this from config file
       DEFAULT_ROOT_DIR
     end
 
+    # The default dry directory for the project.
+    def self.default_dry_dir
+      # TODO: Get this from config file
+      DEFAULT_DRY_DIR
+    end
+
+    # The default live directory for the project.
+    def self.default_live_dir
+      # TODO: Get this from config file
+      DEFAULT_LIVE_DIR
+    end
+
     def self.project_root_dir(jira_ticket)
-      make_project_root_dir(jira_ticket)
+      project_root_dir_for(jira_ticket)
     end
 
-    def self.project_in_work_dir(jira_ticket)
-      project_root_dir = make_project_root_dir(jira_ticket)
-      File.join(project_root_dir, DEFAULT_IN_WORK_DIR)
+    def self.project_dry_dir(jira_ticket)
+      project_root_dir = project_root_dir_for(jira_ticket)
+      File.join(project_root_dir, default_dry_dir)
     end
 
-    def self.project_completed_dir(jira_ticket)
-      project_root_dir = make_project_root_dir(jira_ticket)
-      File.join(project_root_dir, DEFAULT_COMPLETED_DIR)
-    end
-
-    def self.create_project_struct(jira_ticket, project_root_dir, project_subdirs)
-      project = OpenStruct.new
-      project.jira_ticket = normalize_jira_ticket(jira_ticket)
-      project.root_dir = project_root_dir
-      project.in_work_dir = project_subdirs[:in_work_dir]
-      project.completed_dir = project_subdirs[:completed_dir]
-      project
+    def self.project_live_dir(jira_ticket)
+      project_root_dir = project_root_dir_for(jira_ticket)
+      File.join(project_root_dir, default_live_dir)
     end
 
     def self.normalize_jira_ticket(jira_ticket)
       jira_ticket.upcase
     end
 
-    def self.make_project_root_dir(jira_ticket)
+    def self.project_root_dir_for(jira_ticket)
       jira_ticket = normalize_jira_ticket(jira_ticket)
-      File.join(DEFAULT_ROOT_DIR, jira_ticket)
+      File.join(default_root_dir, jira_ticket)
     end
 
+    def self.project_dry_dir_for(jira_ticket)
+      project_root_dir = project_root_dir_for(jira_ticket)
+      File.join(project_root_dir, default_dry_dir)
+    end
+
+    def self.project_live_dir_for(jira_ticket)
+      project_root_dir = project_root_dir_for(jira_ticket)
+      File.join(project_root_dir, default_live_dir)
+    end
+
+    # Expands any ~/whatever to /whatever
     def self.expand_project_root_dir(project_root_dir)
       File.expand_path(project_root_dir)
     end
 
-    def self.create_project_root_dir(jira_ticket)
-      project_root_dir = make_project_root_dir(jira_ticket)
+    def self.create_root_dir(jira_ticket)
+      project_root_dir = project_root_dir_for(jira_ticket)
       project_root_dir = expand_project_root_dir(project_root_dir)
       FileUtils.mkpath(project_root_dir)
       project_root_dir
     end
 
-    def self.create_project_sub_dirs(project_root_dir)
+    def self.create_sub_dirs(project_root_dir)
       project_sub_dirs = {}
 
-      project_sub_dirs[:in_work_dir] =
-        File.join(project_root_dir, DEFAULT_IN_WORK_DIR)
-      project_sub_dirs[:completed_dir] =
-        File.join(project_root_dir, DEFAULT_COMPLETED_DIR)
+      project_sub_dirs[:dry_dir] =
+        File.join(project_root_dir, default_dry_dir)
+      project_sub_dirs[:live_dir] =
+        File.join(project_root_dir, default_live_dir)
 
-      FileUtils.mkpath(project_sub_dirs[:in_work_dir])
-      FileUtils.mkpath(project_sub_dirs[:completed_dir])
+      FileUtils.mkpath(project_sub_dirs[:dry_dir])
+      FileUtils.mkpath(project_sub_dirs[:live_dir])
 
       project_sub_dirs
     end
 
-    private_class_method :create_project_struct
+    def self.create_property_files(jira_ticket, property_ids)
+      property_files = { dry_files: [], live_files: [] }
+      property_ids.each do |property_id|
+        dry_file = create_dry_property_file(jira_ticket, property_id)
+        live_file = create_live_property_file(jira_ticket, property_id)
+        property_files[:dry_files] << dry_file
+        property_files[:live_files] << live_file
+      end
+      property_files
+    end
+
+    def self.create_dry_property_file(jira_ticket, property_id)
+      file = File.join(project_dry_dir_for(jira_ticket),
+                       property_dry_file_name_for(property_id))
+      FileUtils.touch(file)
+      file
+    end
+
+    def self.create_live_property_file(jira_ticket, property_id)
+      file = File.join(project_live_dir_for(jira_ticket),
+                       property_live_file_name_for(property_id))
+      FileUtils.touch(file)
+      file
+    end
+
+    def self.property_dry_file_name_for(property_id)
+      "#{property_id}.dry.txt"
+    end
+
+    def self.property_live_file_name_for(property_id)
+      "#{property_id}.live.txt"
+    end
+
     private_class_method :normalize_jira_ticket
-    private_class_method :make_project_root_dir
+    private_class_method :project_root_dir_for
     private_class_method :expand_project_root_dir
-    private_class_method :create_project_root_dir
-    private_class_method :create_project_sub_dirs
+    private_class_method :project_dry_dir_for
+    private_class_method :project_live_dir_for
+    private_class_method :create_root_dir
+    private_class_method :create_sub_dirs
+    private_class_method :create_property_files
+    private_class_method :property_dry_file_name_for
+    private_class_method :property_live_file_name_for
+
+    private
+
+    attr_writer :jira_ticket
+    attr_writer :property_ids
+    attr_writer :root_dir
+    attr_writer :dry_dir
+    attr_writer :live_dir
+    attr_writer :property_files
   end
 end
--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -6,10 +6,14 @@ RSpec.describe YardiConvertTools::Project, type: :aruba do
   before(:all) { remove_test_files }
   after(:each) { remove_test_files }
 
-  let(:project) { YardiConvertTools::Project.create_project(jira_ticket) }
+  let(:project) { YardiConvertTools::Project.new(jira_ticket, property_ids) }
 
   def jira_ticket
     'XYZ-1234'.freeze
+  end
+
+  def property_ids
+    %w{ 0 1 2 3 4 5 6 7 8 9 }
   end
 
   def remove_test_files
@@ -19,33 +23,83 @@ RSpec.describe YardiConvertTools::Project, type: :aruba do
   def remove_test_dirs
     project_root_dir = YardiConvertTools::Project.project_root_dir(jira_ticket)
     project_subdirs = Dir.glob(File.join(project_root_dir, '*'))
+    project_files = Dir.glob(File.join(project_root_dir, '**/*.{dry.txt,live.txt}'))
+    FileUtils.rm(project_files) unless project_files.empty?
     FileUtils.rmdir(project_subdirs) unless project_subdirs.empty?
     FileUtils.rmdir(project_root_dir) if Dir.exist?(project_root_dir)
   end
 
-  context '.create_project' do
-    it 'should create the project' do
+  context 'attributes' do
+    context '#jira_ticket' do
+      it 'should be the right jira ticket' do
+        expect(project.jira_ticket).to eq(jira_ticket)
+      end
+    end
+
+    context '#property_ids' do
+      it 'should have the right property ids' do
+        expect(project.property_ids).to match_array(property_ids)
+      end
+    end
+
+    context '#root_dir' do
+      it 'should have the right root directory' do
+        expect(project.root_dir).to eq(File.join('/tmp', "#{jira_ticket}"))
+      end
+    end
+
+    context '#dry_dir' do
+      it 'should have the right dry run directory' do
+        expect(project.dry_dir).to eq(File.join('/tmp', "#{jira_ticket}/dry"))
+      end
+    end
+
+    context '#live_dir' do
+      it 'should have the right live run directory' do
+        expect(project.live_dir).to eq(File.join('/tmp', "#{jira_ticket}/live"))
+      end
+    end
+
+    context '#property_files' do
+      it 'should have the right dry run files' do
+        (0..9).each do |index|
+          file = project.property_files[:dry_files][index]
+          expect(file).to eq("/tmp/#{jira_ticket}/dry/#{index}.dry.txt")
+        end
+      end
+
+      it 'should have the right live run files' do
+        (0..9).each do |index|
+          file = project.property_files[:live_files][index]
+          expect(file).to eq("/tmp/#{jira_ticket}/live/#{index}.live.txt")
+        end
+      end
+    end
+  end
+
+  context '#new' do
+    it 'should create the project root directory' do
       expect(project.root_dir).to be_an_existing_path
     end
 
-    it 'should create the project in-work subfolder' do
-      expect(project.in_work_dir).to be_an_existing_path
+    it 'should create the project dry subfolder' do
+      expect(project.dry_dir).to be_an_existing_path
     end
 
-    it 'should create the project completed subfolder' do
-      expect(project.completed_dir).to be_an_existing_path
+    it 'should create the project live subfolder' do
+      expect(project.live_dir).to be_an_existing_path
     end
-  end
 
-  context '.create_property' do
-    it 'should create the property'
-  end
+    it 'should create the dry project files' do
+      project.property_files[:dry_files].each do |dry_file|
+        expect(dry_file).to be_an_existing_file
+      end
+    end
 
-  context '.create_properties' do
-    it 'should create the properties'
-  end
-
-  context '.create_properties' do
-    it 'should create the properties'
+    it 'should create the live project files' do
+      project.property_files[:live_files].each do |live_file|
+        expect(live_file).to be_an_existing_file
+      end
+    end
   end
 end
